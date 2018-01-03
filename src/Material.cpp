91ba2720602d589907bcc001d6b3d18e2882181b
mattkae <mkosare1@gmail.com>
2018-01-03 13:28:09 -0500

    Added two new constructors to the Material class

--- src/Material.cpp
+++ src/Material.cpp
@@ -4,9 +4,29 @@
 const glm::vec3 DEFAULT_EMISSIVE = glm::vec3(1.0, 1.0, 1.0);
 const glm::vec3 DEFAULT_DIFFUSE = glm::vec3(1.0, 1.0, 1.0);
 const glm::vec3 DEFAULT_SPECULAR  = glm::vec3(1.0, 1.0, 1.0);
+const GLfloat DEFAULT_SHININESS = 2.7;
 
 Material::Material() {
   mEmissive = DEFAULT_EMISSIVE;
   mDiffuse = DEFAULT_DIFFUSE;
   mSpecular = DEFAULT_SPECULAR;
+  mShininess = DEFAULT_SHININESS;
+}
+
+Material::Material(glm::vec3 emissive, glm::vec3 diffuse, glm::vec3 specular) {
+  mEmissive = emissive;
+  mDiffuse = diffuse;
+  mSpecular = specular;
+  mShininess = DEFAULT_SHININESS;
+}
+
+Material::Material(glm::vec3 emissive, glm::vec3 diffuse, glm::vec3 specular, GLfloat shininess) {
+  mEmissive = emissive;
+  mDiffuse = diffuse;
+  mSpecular = specular;
+  mShininess = shininess;
+}
+
+void Material::render() {
+  // TO-DO: Implement render
 }
--- src/Material.h
+++ src/Material.h
@@ -7,13 +7,14 @@ class Material {
  public:
   Material();
   Material(glm::vec3 emissive, glm::vec3 diffuse, glm::vec3 specular);
+  Material(glm::vec3 emissive, glm::vec3 diffuse, glm::vec3 specular, GLfloat shininess);
   void render();
 
  private:
   glm::vec3 mEmissive;
   glm::vec3 mDiffuse;
   glm::vec3 mSpecular;
-  GLfloat mShininess;
+  GLfloat mShininess = 0;
 };
 
 #endif
